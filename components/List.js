d93a595c409b08fc6fb412ab1899786430cbfa11
javaapp2020 <javaapp2020@gmail.com>
2021-02-18 13:31:39 +0900

    naviagtion tab to tab sample

--- components/List.js
+++ components/List.js
@@ -31,7 +31,8 @@ const List = ({ navigation }) => {
             // .navigate(스택스크린이름, 매개변수객체)
             onPress={()=>{navigation.navigate("Tasks", {id: item.id})}}
             
-            // tab -> tab 
+            // tab>stack -> tab>stack 
+            // https://reactnavigation.org/docs/nesting-navigators/#passing-params-to-a-screen-in-a-nested-navigator
             // .navigate(탭스크린이름, {params: 매개변수객체, screen: 스택스크린이름})
             // onPress={()=>{navigation.navigate("Tasks", {params: {id: item.id}, screen: "Tasks"})}}
             >
