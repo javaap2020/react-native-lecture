48099c4f71e3db89967a0acaf930ff95ecaf2962
javaapp2020 <javaapp2020@gmail.com>
2021-03-05 12:09:39 +0900

    gelocation

---
+++ components/HWTest.js
@@ -0,0 +1,57 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { Button, Platform, PermissionsAndroid  } from 'react-native';
+import { Text, View } from 'react-native';
+
+// https://github.com/Agontuk/react-native-geolocation-service
+import Geolocation from 'react-native-geolocation-service';
+
+const HWTest = () => {
+  const [location, setLocation] = useState({});
+  console.log("--location--");
+  console.log(location);
+
+  // 위치정보 권한요청 메소드
+  const requestPermissions = useCallback(async ()=>{
+    if (Platform.OS === 'ios') {
+      Geolocation.requestAuthorization();
+      Geolocation.setRNConfiguration({
+        skipPermissionRequests: false,
+        authorizationLevel: 'whenInUse',
+     });
+    }
+  
+    if (Platform.OS === 'android') {
+      await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      );
+    }    
+  }, [])
+  
+  const getLocation = useCallback(()=>{
+    Geolocation.getCurrentPosition(
+      (position) => {
+        console.log("--position--");
+        console.log(position);
+        setLocation(position.coords);
+      },
+      (error) => {
+        // See error code charts below.
+        console.log(error.code, error.message);
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+    );
+  }, [])
+  
+  useEffect(()=>{
+    requestPermissions();
+  }, [])  
+
+  return (
+    <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+      <Button color="tomato" onPress={()=>{getLocation()}} title="Get Location" />
+      <Text>lat: {location.latitude}</Text>
+      <Text>lng: {location.longitude}</Text>
+    </View>
+  )
+}
+export default HWTest;
